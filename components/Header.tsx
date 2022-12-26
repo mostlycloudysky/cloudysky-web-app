65f59c19141e2a767ad904ffddc65c5257b3a79f
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2022-12-26 08:45:52 -0500

    add more to nav bar items

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <div className=' flex items-center space-x-5'>
+        <Link href='/'>
+            <div className='flex items-center flex-shrink-0 py-5'>
+              <img src='./developer.png' alt='logo' className='w-12 h-12 object-contain cursor-pointer' />   
+              <span className='pl-2 font-bold text-black text-xl'>
+                CloudyS.K.Y
+              </span>
+            </div>
+        </Link>
+        <div className='flex items-center space-x-5'>
+          <h3>Home</h3>
+          <h3>Blogs</h3>
+          <h3>Snippets</h3>
+          <h3 className='border px-4 py-1 rounded-full border-green-600'>Visitors Log</h3>
+        </div>
+      </div>
+    </header>
   )
 }
 
