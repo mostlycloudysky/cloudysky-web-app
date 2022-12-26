01b28704efa045d336e36d32e5e0a1edabf21e6b
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2022-12-26 14:51:37 -0500

    improve nav bar items

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import Link from 'next/link'
-import { BeakerIcon, SunIcon } from '@heroicons/react/24/solid'
+import { SunIcon } from '@heroicons/react/24/solid'
 import { useRouter } from 'next/router';
+import cn from 'classnames';
 
+interface Props {
+  href: string,
+  text: string
+}
+
+function NavItem({href, text}: Props) {
+  const router = useRouter()
+  const isActive = router.asPath === href
+
+  return (
+    <Link 
+      href={href}
+      className={cn(isActive ? 'font-semibold text-green-600': ' font-normal', 'text-gray-700 cursor-pointer hover:text-green-600')} >
+      <span className="">{text}</span>
+    </Link>
+  )
+}
 
 function Header() {
   const router = useRouter();
@@ -10,17 +28,15 @@ function Header() {
     <>
      <header className='flex items-center justify-between pl-5 pt-5 pb-2 pr-5'>
         <div className='flex items-center space-x-5 flex-shrink-0 py-5'>
-          <Link href='/' className='text-gray-700 cursor-pointer hover:text-green-600'>Home</Link>
-          <Link href='/blogs' className='text-gray-700 hover:text-green-600 cursor-pointer'>Blogs</Link>
-          <Link href='/snippets' className='text-gray-700 hover:text-green-600 cursor-pointer'>Snippets</Link>
-          <Link href='/visitor' className='text-gray-700 hover:text-green-600 cursor-pointer'>Visitors Log</Link>
+          <NavItem href='/' text='Home' />
+          <NavItem href='/blogs' text='Blogs' />
+          <NavItem href='/snippets' text='Snippets' />
+          <NavItem href='/visitor' text='Visitors Log' />   
         </div>
         <div className='flex items-center space-x-4'>
           <h3 className=' border px-3 rounded-full py-1  cursor-pointer  border-green-600 text-green-600 hover:bg-green-600 hover:text-white  '>Getting Started</h3>
           <SunIcon className='h-6 w-6 text-yellow-500' />
         </div>
-      
-      {/* </div> */}
     </header>
     <div className='border-b-[1px] border-muted ml-5 mr-5'></div>
     </>
