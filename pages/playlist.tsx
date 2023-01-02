0351c85a0553b620810186ca23e4c162bcba77ed
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2023-01-01 21:11:57 -0500

    fix playlist page routing

---
+++ pages/playlist.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout';
+import Header from '../components/Header';
+
+function playlist() {
+  return (
+    <Layout title='CloudyS.K.Y - Playlist'>
+      <Header />
+      <h1>This is playlist page</h1>
+    </Layout>
+  )
+}
+
+export default playlist
