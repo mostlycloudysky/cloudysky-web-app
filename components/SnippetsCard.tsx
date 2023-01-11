40290aba7bbfadfe85d6f3bb2b643aa95aa33cc9
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2023-01-11 11:29:36 -0500

    add snippets to home page

--- components/SnippetsCard.tsx
+++ components/SnippetsCard.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { Snippets } from '../typings'
+import { urlFor } from '../sanity'
 
-function SnippetsCard() {
+
+interface Props {
+    snippet: Snippets
+}
+
+function SnippetsCard({snippet}: Props) {  
   return (
     <div className="mb-5 cursor-pointer ">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 lg:gap-8">
+        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 lg:gap-8"> */}
             <div className="border border-grey-200 dark:border-gray-800 rounded p-4 w-full bg-gray-50 dark:bg-gray-900 hover:scale-105 transition-transform duration-200 ease-in-out">
-                <img src='./avatar.jpg' alt='logo' className=' w-10 h-10 rounded-full ' />
+                <img src={urlFor(snippet.mainImage).url()!} alt='logo' className=' w-10 h-10 rounded-full ' />
                 <h3 className="text-lg font-bold text-left mt-2 text-gray-900 dark:text-gray-100">
-                    Serverless Code
+                    {snippet.title}
                 </h3>
-                <p className="mt-1 text-gray-700 dark:text-gray-400">Lorem ipsum dolor</p>
+                <p className="mt-1 text-gray-700 dark:text-gray-400">{snippet.description}</p>
             </div>
-            <div className="border border-grey-200 dark:border-gray-800 rounded p-4 w-full bg-gray-50 dark:bg-gray-900 hover:scale-105 transition-transform duration-200 ease-in-out">
-                <img src='./avatar.jpg' alt='logo' className=' w-10 h-10 rounded-full ' />
-                <h3 className="text-lg font-bold text-left mt-2 text-gray-900 dark:text-gray-100">
-                    Serverless Code
-                </h3>
-                <p className="mt-1 text-gray-700 dark:text-gray-400">Lorem ipsum dolor</p>
-            </div>
-        </div>
+        {/* </div> */}
     </div>
   )
 }
--- typings.d.ts
+++ typings.d.ts
@@ -22,6 +22,23 @@ export interface Post {
     body: [object];
 }
 
+export interface Snippets {
+    _id: string;
+    _createdAt: string;
+    title: string;
+    // comments: Comment[];
+    description: string;
+    mainImage: {
+      asset: {
+        url: string;
+      };
+    };
+    slug: {
+      current: string;
+    };
+    body: [object];
+}
+
 export interface DetailedPost {
     _id: string;
     _createdAt: string;
