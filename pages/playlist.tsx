f039901dbf4ee71b69d6fee721d71d647b1da8cf
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2023-02-13 19:37:37 -0500

    Update playlist.tsx

--- pages/playlist.tsx
+++ pages/playlist.tsx
@@ -19,58 +19,6 @@ export default function playlist({playListData}) {
         <PlayListHeader data={data} />
         <Songs songs={data} />
         <Footer />
-          {/* <div className="mt-10">
-            <div className="flex text-gray-600">
-              <div className="p-2 w-8 flex-shrink-0"></div>
-              <div className="p-2 w-8 flex-shrink-0"></div>
-              <div className="p-2 w-full">Title</div>
-              <div className="p-2 w-full">Artist</div>
-              <div className="p-2 w-full">Album</div>
-              <div className="p-2 w-12 flex-shrink-0 text-right">⏱</div>
-            </div>
-    
-    <div className="flex border-b border-gray-800 hover:bg-gray-800">
-      <div className="p-3 w-8 flex-shrink-0">▶️</div>
-      <div className="p-3 w-8 flex-shrink-0">❤️</div>
-      <div className="p-3 w-full">My Song Here</div>
-      <div className="p-3 w-full">Eminem</div>
-      <div className="p-3 w-full">Spotify</div>
-      <div className="p-3 w-12 flex-shrink-0 text-right">5:35</div>
-    </div>
-    <div className="flex border-b border-gray-800 hover:bg-gray-800">
-      <div className="p-3 w-8 flex-shrink-0">▶️</div>
-      <div className="p-3 w-8 flex-shrink-0">❤️</div>
-      <div className="p-3 w-full">My Song Here</div>
-      <div className="p-3 w-full">Eminem</div>
-      <div className="p-3 w-full">Spotify</div>
-      <div className="p-3 w-12 flex-shrink-0 text-right">5:35</div>
-    </div>
-    <div className="flex border-b border-gray-800 hover:bg-gray-800">
-      <div className="p-3 w-8 flex-shrink-0">▶️</div>
-      <div className="p-3 w-8 flex-shrink-0">❤️</div>
-      <div className="p-3 w-full">My Song Here</div>
-      <div className="p-3 w-full">Eminem</div>
-      <div className="p-3 w-full">Spotify</div>
-      <div className="p-3 w-12 flex-shrink-0 text-right">5:35</div>
-    </div>
-    <div className="flex border-b border-gray-800 hover:bg-gray-800">
-      <div className="p-3 w-8 flex-shrink-0">▶️</div>
-      <div className="p-3 w-8 flex-shrink-0">❤️</div>
-      <div className="p-3 w-full">My Song Here</div>
-      <div className="p-3 w-full">Eminem</div>
-      <div className="p-3 w-full">Spotify</div>
-      <div className="p-3 w-12 flex-shrink-0 text-right">5:35</div>
-    </div>
-    <div className="flex border-b border-gray-800 hover:bg-gray-800">
-      <div className="p-3 w-8 flex-shrink-0">▶️</div>
-      <div className="p-3 w-8 flex-shrink-0">❤️</div>
-      <div className="p-3 w-full">My Song Here</div>
-      <div className="p-3 w-full">Eminem</div>
-      <div className="p-3 w-full">Spotify</div>
-      <div className="p-3 w-12 flex-shrink-0 text-right">5:35</div>
-    </div>
-          </div> */}
-
       </div>
     </Layout>
   )
