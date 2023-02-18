b4cfab24a34d69c6f69f4589dd0de880d16363cc
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2023-02-18 08:30:24 -0500

    Update playlist.tsx

--- pages/playlist.tsx
+++ pages/playlist.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout';
-import Header from '../components/Header';
-import Toptracks from '../components/Toptracks';
 import { useState, useEffect} from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
 import PlayListHeader from '../components/PlayListHeader';
 import Songs from '../components/Songs';
 import Footer from '../components/Footer';
