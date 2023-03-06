f0fbe82f73dacbeae348b19e8c1675c9576e9a46
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2023-03-05 21:30:24 -0500

    more updates

--- pages/api/opensearch-results.tsx
+++ pages/api/opensearch-results.tsx
@@ -1,4 +1,6 @@
 import {getSearchResults} from '../../lib/opensearch'
+import enablePublicAccess from '../../cors-middleware'
+
 
 export default async function handler(req, res) {
     const {q} = req.query;
