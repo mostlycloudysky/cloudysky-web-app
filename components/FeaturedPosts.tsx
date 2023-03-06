90fce477d460f7019c1b81030513745572fc07c3
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2023-03-05 20:57:34 -0500

    Complete Search Functionality

--- components/FeaturedPosts.tsx
+++ components/FeaturedPosts.tsx
@@ -25,8 +25,9 @@ function FeaturedPosts({ post }: Props) {
           </p>
           <div className='mt-2 flex items-center justify-between'>
             <div className='flex items-center space-x-2'>
-              {post.categories.map((catagory) => (
+              {post.categories.map((catagory, index) => (
                 <span
+                    key={index}
                     className="inline-flex items-center leading-none px-2.5 py-1.5 text-sm rounded-full border border-gray-600"
                       >
                       {catagory.title}
